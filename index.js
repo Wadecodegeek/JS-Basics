cd677019e3b989f97db2c363bfaf2cde7c4ba640
Wadecodegeek <90144697+Wadecodegeek@users.noreply.github.com>
2023-04-05 15:22:07 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,93 @@
+//First JS code
+
+//console.log('Hello World');
+
+   
+
+let name = 'Wade';
+
+console.log(name);  
+
+//Rules(names) : Cannot be a reserved keyword
+
+//Should be meaningful
+
+//Cannot start with a number (1name)
+
+//Cannot contain a spaceor hyphen (-)
+
+//Are case-sensitive
+
+ 
+
+let firstName = 'Wade';
+
+ 
+
+let lastName ='Nenhowe';  
+
+console.log  (firstName + ' '+ lastName);
+
+ 
+
+let interestRate = 0.3;
+
+interestRate = 1;
+
+console.log(interestRate);
+
+ 
+
+//Primitives/Value Types
+
+//let name = 'Wade'; // String Literal
+
+let age = 30;// Number Literal
+
+let isApproved = true; //Boolean Literal
+
+//let firstName = 'undefined';
+
+//let lastName = null
+
+ 
+
+let selectedColors = ['red', 'blue'];
+
+selectedColors[2] = '15';
+
+console.log(selectedColors.length);
+
+ 
+
+//Performing a task
+
+function greet(name) {
+
+console.log('Hello '+ name + ' ' + lastName );
+
+}
+
+greet ('Wade','Nenhowe' );
+
+greet ('Tracey-Lee');
+
+greet ('Connor');
+
+greet ('Mom');
+
+greet ('Tania');
+
+ 
+
+//Calculating a value
+
+function square(number) {
+
+return number*number;
+
+}
+
+let number = square();
+
+console.log(number);
